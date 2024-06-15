dd5f886847ba36cdc17c3c54d2470ecaa49a78e9
damzymichael <105460672+damzymichael@users.noreply.github.com>
2024-06-15 17:12:25 +0100

    Progress

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -12,31 +12,47 @@ function scrollToTop() {
 }
 
 let ctx: gsap.Context;
-onMounted(() => {
-  window.addEventListener('beforeunload', scrollToTop);
 
-  gsap.from('h2 span', {
-    opacity: 0,
-    left: '-200px',
-    ease: 'power3.Out',
-    stagger: 0.1,
-    delay: 0.7
-  });
+onMounted(() => {
+  // window.addEventListener('beforeunload', scrollToTop);
 
   //61px height of navigation
   ctx = gsap.context(() => {
-    gsap.to('h2 span', {
-      scrollTrigger: {
-        trigger: 'h2 span',
-        start: 'top 61px',
-        end: 'bottom top',
-        scrub: true
-      },
-      immediateRender: false,
+    gsap.from('h2 span', {
       opacity: 0,
-      left: '200px',
+      left: '-200px',
       ease: 'power3.Out',
-      stagger: -0.1
+      stagger: 0.1,
+      delay: 0.7
+    });
+
+    // gsap.to('h2 span', {
+    //   scrollTrigger: {
+    //     trigger: 'h2 span',
+    //     start: 'top 61px',
+    //     end: 'bottom top',
+    //     scrub: true
+    //   },
+    //   immediateRender: false,
+    //   opacity: 0,
+    //   left: '200px',
+    //   ease: 'power3.Out',
+    //   stagger: -0.1
+    // });
+
+    //Hero section 2 animation
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.hero2',
+        start: 'top center'
+      },
+      yoyo: true
+    });
+    timeline.from('.hero2', {backgroundSize: '120%'});
+    timeline.to('.banner2', {
+      opacity: 1,
+      left: 0,
+      stagger: 0.1
     });
   });
 });
@@ -54,7 +70,9 @@ onUnmounted(() => {
       class="hero h-screen bg-[#313133] -m-5 sm:-m-10 p-5 sm:p-10 pb-0 flex items-end"
     >
       <div class="mb-[20vh]">
-        <h2 class="uppercase mb-3 text-3xl sm:text-4xl overflow-x-hidden">
+        <h2
+          class="uppercase relative mb-3 text-3xl sm:text-4xl overflow-x-hidden"
+        >
           <span class="block relative">Customize your </span>
           <span class="block relative">merch, the way you</span>
           <span class="block relative">want it</span>
@@ -98,16 +116,18 @@ onUnmounted(() => {
       <div
         class="hero2 mb-2 min-h-[31rem] sm:min-h-96 p-5 sm:p-10 flex flex-col justify-end items-start"
       >
-        <h1 class="uppercase text-2xl sm:text-4xl w-full sm:w-3/4 mb-3">
+        <h1 class="banner2 uppercase text-2xl sm:text-4xl w-full sm:w-3/4 mb-3">
           Start customizing your <br />
           own merch now.
         </h1>
-        <p class="mb-4">
+        <p class="banner2 mb-4">
           We have made the process really easy to use to get your desired merch
         </p>
-        <Button>
-          <span class="text-sub/500">Get started now</span>
-        </Button>
+        <div class="banner2">
+          <Button>
+            <span class="text-sub/500">Get started now</span>
+          </Button>
+        </div>
       </div>
     </section>
   </main>
@@ -124,8 +144,14 @@ section.hero {
 div.hero2 {
   background: url('../assets/merch-boy.png');
   background-repeat: no-repeat;
-  background-size: cover;
+  background-size: 100%;
   background-position: center;
+}
+
+.banner2 {
+  position: relative;
+  opacity: 0;
+  left: -200px;
 }
 
 @media screen and (max-width: 640px) {
