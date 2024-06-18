9511157be73d07c51ec134b8bd39cb0608389adc
damzymichael <105460672+damzymichael@users.noreply.github.com>
2024-06-18 02:19:22 +0100

    Comples animation set up

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -10,19 +10,31 @@ defineProps<{products: Product[]}>();
 onMounted(() => {
   gsap.defaults({ease: 'power3'});
 
-  // gsap.set('.product', {y: 100, opacity: 0});
+  gsap.set('.product', {y: 100});
 
   ScrollTrigger.batch('.product', {
-    start: 'top bottom-=100px',
-    onEnter: batch => gsap.to(batch, {opacity: 1, y: 0, stagger: 0.15}),
-    onLeaveBack: batch => gsap.to(batch, {opacity: 0, y: 100, stagger: 0.1})
+    // start: 'top bottom-=100px',
+    start: '20px bottom',
+    end: 'top -=150px',
+    onEnter: batch =>
+      gsap.to(batch, {opacity: 1, y: 0, stagger: 0.15, overwrite: true}),
+    onLeave: batch =>
+      gsap.set(batch, {opacity: 0, y: -100, stagger: 0.15, overwrite: true}),
+    onEnterBack: batch =>
+      gsap.to(batch, {opacity: 1, y: 0, stagger: 0.15, overwrite: true}),
+    onLeaveBack: batch =>
+      gsap.set(batch, {opacity: 0, y: 100, stagger: 0.1, overwrite: true})
+  });
+
+  ScrollTrigger.addEventListener('refreshInit', () => {
+    gsap.set('.box', {y: 0});
   });
 });
 </script>
 
 <template>
   <div class="product-container flex gap-1 justify-center flex-wrap mb-10">
-    <figure v-for="product in products" class="product w-[49%] sm:w-[33%] mb-4 opacity-0 translate-y-[100px]">
+    <figure v-for="product in products" class="product w-[49%] sm:w-[33%] mb-4">
       <img :src="product.image" alt="" class="mb-2" />
       <figcaption class="text-main/900">
         <h4>{{ product.name }}</h4>
